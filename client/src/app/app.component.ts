14a98b41a668a1b94d0b56e00f8876b29f30d522
devjos <github_11837948@feido.de>
2018-06-09 01:22:00 +0200

    mvp

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import geojson2svg from 'geojson2svg';
 
 @Component({
   selector: 'app-root',
@@ -10,13 +9,34 @@ export class AppComponent {
   title = 'app';
 
   ngOnInit() {
-    var rawImage = "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==";
-    document.getElementById("map").src = rawImage;
 
-    var converter = geojson2svg();
-    var geojson = JSON.parse('{"type":"Polygon","coordinates":[[[3,1],[4,4],[2,4],[1,2],[3,1]]]}');
-    var svgStrings = converter.convert(geojson);
-    console.log(svgStrings[0]);
-    document.getElementById("overlay").innerHTML = '<path d="M150 0 L75 200 L225 200 Z" />';
-  }
+    var points = [
+      {x: 10, y: 30},
+      {x: 40, y: 40},
+      {x: 80, y: 40},
+      {x: 100, y: 40},
+      {x: 130, y: 40},
+      {x: 150, y: 40},
+      {x: 170, y: 40},
+    ];
+
+    var min=0;
+
+    setInterval((){
+      console.log(points);
+      var svg = ''
+      var i;
+      for (i = min; i < points.length; i++) {
+          svg += '<circle cx="' + points[i].x + '" cy="' + points[i].y + '" r="5" fill="red" />'
+      }
+
+      for (i = min; i < points.length - 1; i++) {
+          svg += '<line x1="'+points[i].x+'" y1="'+points[i].y+'" x2="'+points[i+1].x+'" y2="'+points[i+1].y+'" style="stroke:rgb(255,0,0);stroke-width:2" />'
+      }
+
+      document.getElementById("overlay").innerHTML = svg;
+      //min++;
+
+    }, 1000);
+
 }
