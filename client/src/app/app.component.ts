e74f0c3a955d5d75f409627a571edf0864b82a5f
devjos <github_11837948@feido.de>
2018-06-09 09:14:21 +0200

    current status

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -8,38 +9,39 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'app';
   min = 0;
-  points = [];
+  products = [];
+  product = "";
+  configUrl = 'assets/config.json';
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
-    this.points = [
-      {x: 0, y: 0},
-      {x: 400, y: 350},
-      {x: 600, y: 400},
-    ];
-    this.printRoute();
+
+    var rawImage = "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==";
+    document.getElementById("map").src = rawImage;
   }
 
-  printRoute(){
-    var svg = ''
-    var i;
-    for (i = this.min; i < this.points.length; i++) {
-        svg += '<circle cx="' + this.points[i].x + '" cy="' + this.points[i].y + '" r="5" fill="red" />'
-    };
-
-    for (i = this.min; i < this.points.length - 1; i++) {
-        svg += '<line x1="'+this.points[i].x+'" y1="'+this.points[i].y+'" x2="'+this.points[i+1].x+'" y2="'+this.points[i+1].y+'" style="stroke:rgb(255,0,0);stroke-width:2" />'
-    };
-
-    document.getElementById("overlay").innerHTML = svg;
+  addProduct(val){
+    this.products.push(val);
   }
 
   back() {
     this.min -= 1;
-    this.printRoute();
+    this.updateMap();
   }
   next() {
     this.min += 1;
-    this.printRoute();
+    this.updateMap();
+  }
+
+  delete(index){
+    this.products.splice(index, 1);
+  }
+
+  updateMap(){
+    this.http.post(this.configUrl).subscribe(data => {
+      console.log(data);
+    });
   }
 
 }
